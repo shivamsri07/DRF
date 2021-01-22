c673f66c546e0f36051af1cf4b1dada18a2549cb
ShivamSrivastava <7shivamsrivastava@gmail.com>
2021-01-22 15:17:44 +0530

    Create Authentication System

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api_basic',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication',
+    ]
+}
+
 
 
 # Internationalization
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api_basic.url'))
+    path('', include('api_basic.url')),
+    path('', include('user.url')),
 ]
